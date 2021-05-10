5c63d1d9421ffc085a9f8f2073b386d0337cff7d
melissa-koi <melissawangui3@gmail.com>
2021-05-10 10:02:17 +0300

    add forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,14 @@ from wtforms import StringField,TextAreaField,SubmitField, SelectField
 from wtforms.validators import Required
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('Content', validators=[Required()])
+    submit = SubmitField('Post')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Post')
